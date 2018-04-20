ddc53b8564fbf00d5a0b73cc261d7dd339a60ab4
Emily Jirles <emily.jirles@gmail.com>
2018-04-20 15:11:43 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+// declare your functions here...
+function paragraphSelector(){
+  //select and return all p tags
+  $('p')
+}
+
+function lastImageSelector(){
+  //returns last image
+  $('img:last')
+}
+
+function ninjaBabySelector(){
+  //use id selector to get baby gif
+  $('#baby-ninja')
+}
+
+function divSelector(){
+  //use class selector to get divs labelled pics
+  $('.pics')
+}
+
+function firstListItem(){
+  //use first child selector to get first item in ul with ID pic-list
+  $('#pic-list li:first-child')
+}
